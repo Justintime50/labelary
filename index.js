e6732faed00789015a98b55640b4067149bde2b8
Justintime50 <justinpaulhammond@gmail.com>
2020-02-24 16:57:56 -0700

    First commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const questions  = require("./lib/questions");
+const general = require("./lib/labelary");
+
+// Run the main program flow
+const run = async () => {
+    console.log("Labelary: ZPL => PDF/PNG");
+
+    const askGeneralQuestions = await questions.askGeneralQuestions();
+    general.useGeneralQuestions(askGeneralQuestions);
+
+    console.log("Label generated");
+};
+
+run();
